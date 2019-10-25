0112f0c59a1bd50ac17957bffd05318433482242
WelberthMagalhaes <welberthheider@gmail.com>
2019-10-25 01:33:01 -0300

    JS addCompetidor

--- CSI477-2019-02-atividade-pratica-001/js/corrida.js
+++ CSI477-2019-02-atividade-pratica-001/js/corrida.js
@@ -1,4 +1,32 @@
+var largadas = [];
+var nomes = [];
+var tempos = [];
+
 function addCompetidor() {
-  
+  let largada = document.dados.pLargada.value;
+  let nome = document.dados.nomeCompetidor.value;
+  let tempo = document.dados.tempo.value;
+
+
+
+  //adicionando competidor na lista de competidores
+  largadas.push(largada);
+  nomes.push(nome);
+  tempos.push(tempo);
+
+  //criar elementos
+  var linha = document.createElement("tr");
+  var campoLargada = document.createElement("td");
+  var campoNome = document.createElement("td");
+  var campoTempo = document.createElement("td");
+
+  //Criar nós e vincular aos elementos
+  campoLargada.appendChild(document.createTextNode(largada));
+  campoNome.appendChild(document.createTextNode(nome));
+  campoTempo.appendChild(document.createTextNode(tempo));
+  linha.appendChild(campoLargada);
+  linha.appendChild(campoNome);
+  linha.appendChild(campoTempo);
+  document.getElementById("corpoTabelaLargada").appendChild(linha);
 
 }
