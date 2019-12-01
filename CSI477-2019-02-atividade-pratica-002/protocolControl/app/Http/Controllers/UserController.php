c4d581c54be3c08d24777d7ff2a055e03f05ea10
WelberthMagalhaes <welberthheider@gmail.com>
2019-12-01 03:33:43 -0300

    Area do Usuario Formada - implementar agora

--- CSI477-2019-02-atividade-pratica-002/protocolControl/app/Http/Controllers/UserController.php
+++ CSI477-2019-02-atividade-pratica-002/protocolControl/app/Http/Controllers/UserController.php
@@ -7,5 +7,100 @@ use App\User;
 
 class UserController extends Controller
 {
-  //
+  /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function index()
+   {
+       // $users = User::orderBy('nome')->get();
+       // return view ('users.index', [ 'users' => $users]);
+   }
+
+   /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function create()
+   {
+       $estados = Estado::orderBy('nome')->get();
+       return view('users.create', ['estados' => $estados]);
+   }
+
+   /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+   public function store(Request $request)
+   {
+       //dd($request); //faz o dump(printf da variavel e encerra
+
+       //Validação
+
+       //Gravar
+       //Opção1
+       // $c = new User;
+       // $c->nome = $request->nome;
+       // $c->estado_id = $request->estado_id;
+       // $c->save();
+
+       //Opção2
+       User::create($request->all());//nome das tags de entrada com mesmo nome do BD
+
+       //return redirect('/users');
+       return redirect()->route('users.index');
+
+   }
+
+   /**
+    * Display the specified resource.
+    *
+    * @param  \App\User  $user
+    * @return \Illuminate\Http\Response
+    */
+   public function show(User $user)
+   { //public function show($id){ $user = User::find($id); }
+       //dd($user);
+       return view('users.show', ['user' => $user]);
+   }
+
+   /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  \App\User  $user
+    * @return \Illuminate\Http\Response
+    */
+   public function edit(User $user)
+   {
+     $estados = Estado::orderBy('nome')->get();
+     return view('users.edit', ['estados' => $estados, 'user' => $user]);
+
+   }
+
+   /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\User  $user
+    * @return \Illuminate\Http\Response
+    */
+   public function update(Request $request, User $user)
+   {
+       dd($request);
+   }
+
+   /**
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\User  $user
+    * @return \Illuminate\Http\Response
+    */
+   public function destroy(User $user)
+   {
+       //
+   }
 }
--- CSI477-2019-02-atividade-pratica-002/protocolControl/resources/views/auth/users_area/index.blade.php
+++ CSI477-2019-02-atividade-pratica-002/protocolControl/resources/views/auth/users_area/index.blade.php
@@ -2,7 +2,42 @@
 
 @section('conteudo')
 <!-- TELA ÁREA DE USUÁRIO LOGADO -->
-
-
+<div class="container wrapper">
+  <div class="card-deck">
+    <div class="card text-center">
+      <!-- <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card"> -->
+      <div class="card-header">Novo Usuário</div>
+      <div class="card-body">
+        <p class="card-text">Realizar cadastro de novos usuários</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Acessar</a>
+        <!-- <small class="text-muted">Atualizados 3 minutos atrás</small> -->
+      </div>
+    </div>
+    <div class="card text-center">
+      <!-- <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card"> -->
+      <div class="card-body">
+        <h5 class="card-title">Incluir Requerimento</h5>
+        <p class="card-text">Este é um card com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Acessar</a>
+        <!-- <small class="text-muted">Atualizados 3 minutos atrás</small> -->
+      </div>
+    </div>
+    <div class="card text-center">
+      <!-- <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card"> -->
+      <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este card tem o conteúdo ainda maior que o primeiro, para mostrar a altura igual, em ação.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Acessar</a>
+        <!-- <small class="text-muted">Atualizados 3 minutos atrás</small> -->
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
